7cee69d095501c7a66d4b4cbe0fbe592ad699a65
Rolando Oxalc <qj.oxalc@gmail.com>
2021-10-26 21:09:29 -0500

    agregada fakestoreapi para generar productos de la tienda

--- src/components/products/Promise.js
+++ src/components/products/Promise.js
@@ -1,21 +1,30 @@
-import { products } from "./Products";
+import { useState, useEffect } from "react";
 import { Item } from "./StoreItems/Item";
-const Promises = ([{products}]) => {
-    const productsResponse = new Promise((resolve, reject) => {
-    setTimeout(function () {
-        resolve(products);
-        }, 3000);            
-    });
 
-    productsResponse.then((result) => {
-        JSON.stringify(result);
-        console.log(`Result: ${result}`);
-        }) 
+const Promises = () => {
+    const [product, setProduct] = useState([]);
+    const storeItem = () => {
+        fetch('https://fakestoreapi.com/products?limit=5')
+            .then(res => res.json())
+            .then((json) => {
+                console.log(json);
+                setProduct(json);
+            })
+            .catch(err => {console.error(err);});
+    }
+
+    useEffect(() => {
+        storeItem();
+    }, []);
 
     return(   
         <div className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5 ">
-            {products.map((products) => <Item key={products.id} image={products.image} price={products.price} title={products.title} description={products.description} image={products.image} />)}
+            {product.map((item) => 
+            <Item key={item.id} image={item.image} price={item.price} title={item.title} description={item.description} image={item.image} />)}
         </div>);
+        // <div> 
+        //     <p>product</p> 
+        // </div>);
 };
 
 export {Promises}
