12165c40ffab573d57920014310c70197aeacdf5
Rolando Oxalc <qj.oxalc@gmail.com>
2021-10-28 17:29:41 -0500

    Agregado filtro de categorías

--- src/components/products/Categories/CategoryList.js
+++ src/components/products/Categories/CategoryList.js
@@ -1,11 +1,15 @@
-import { Products } from "../Products"
-
 function CategoryList({product}) {
-    const filteredProducts = Object.values(Products).filter(product => product.category)
+    const filteredProducts = [];
+    product.map(item => {
+    if (filteredProducts.indexOf(item.category) === -1) {
+        filteredProducts.push(item.category)
+    }
+}); 
     return (
         <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
             {filteredProducts.map((item) => 
-            (<a key={item.id} href="#" className="block font-medium text-gray-500 dark:text-gray-300 hover:underline">{item.category}</a>))}
+            (<a key={item.id} href="#" className="block font-medium text-gray-500 dark:text-gray-300 hover:underline">
+                {item}</a>))}
         </div>
     )
     
